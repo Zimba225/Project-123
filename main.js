fd72b58367b06e98af2fbb9af538f668457ac948
Raoo_Goswami <102171850+Zimba225@users.noreply.github.com>
2023-10-30 16:58:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function setup()
+{
+    canvas = createCanvas(350, 350);
+    canvas.position(560, 150);
+}
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(350, 300);
+
+    canvas = createCanvas(550, 500);
+    canvas.position(560, 150);
+    canvas = createCanvas(350, 350);
+    canvas.position(560, 150);
+}
